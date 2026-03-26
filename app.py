912f182546e1ec01d4a5fc3d4f0c5fd86f8fd0be
hsl174639 <156890177+hsl174639@users.noreply.github.com>
2026-03-26 09:45:34 +0800

    e'we

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ async def process_details(files: List[UploadFile] = File(...)):
                     row += 1
                 
                 row += 3
-                worksheet.write(row, 0, "==== 详细充电流水 ====", title_fmt)
+                worksheet.write(row, 0, "Detailed Report", title_fmt)
                 row += 2
                 for vehicle, grp in comp_df.groupby('Vehicle_Email'):
                     worksheet.merge_range(row, 0, row, 1, "Vehicle / Driver Email:", header_green)
